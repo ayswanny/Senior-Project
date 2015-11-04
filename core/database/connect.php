8dfc0078706cee481cea2021faca96e249e0da61
Adam <jasper00>
2015-11-03 21:20:18 -0500

    changed back to 'mysqli' because I couldn't connect

--- core/database/connect.php
+++ core/database/connect.php
@@ -3,7 +3,7 @@
 	//	This will provide initial connection to the database
 	// 	Using WAMP and phpmyadmin, you must provide your own
 	//  login (usually root) and a password that you choose for development
-	$db = new mysql('localhost', 'root', 'qweasdzxc', 'rowanprep');
+	$db = new mysqli('localhost', 'root', 'qweasdzxc', 'rowanprep');
 	if($db->connect_errno) {
 		die("Could not connect to database");
 	}
