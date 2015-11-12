d3db97312a7f02ae4c51b4775d91311e0b260be4
Swanny <swanso52@students.rowan.edu>
2015-11-12 04:35:10 -0500

    fixed database error on edit-add-student.php

--- templates/add-user-form-content.php
+++ templates/add-user-form-content.php
@@ -8,45 +8,31 @@
 
             <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
                 
-            <form id="adduserform" class="form-horizontal" role="form" action="add_user.php" method="post">
+            <form id="adduserform" class="form-horizontal" role="form" action="core/database/add-user.php" method="post">
                 
-                <!-- Username -->
                 <div style="margin-bottom: 25px" class="input-group">
                             <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
                             <input id="login-username" type="text" class="form-control" name="username" value="" placeholder="username">                                        
                         </div>
-                <!-- Password -->
-                <div style="margin-bottom: 25px" class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                            <input id="login-password" type="password" class="form-control" name="password" placeholder="password">
-                        </div>
-                <!-- Email -->
                 <div style="margin-bottom: 25px" class="input-group">
                             <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
                             <input id="login-email" type="text" class="form-control" name="email" placeholder="email">
                 </div>
-                <!-- Type -->
                 <div class="form-group">
                     <label class="col-md-4 control-label" for="selectbasic">Account Type</label>
                     <div class="col-md-5">
-                      <select id="selectbasic" name="teacher_type" class="form-control">
+                      <select id="selectbasic" name="admin" class="form-control">
                         <option value="0">User</option>
                         <option value="1">Administrator</option>
                       </select>
                     </div>
                 </div>
-                
-               
+                <div style="margin-top:10px" class="form-group">
+                    <div class="col-sm-12 controls">
+                      <input id="submit" name="submit" type="submit" value="Submit" class"btn btn-primary"></a>
 
-
-                    <div style="margin-top:10px" class="form-group">
-                        <!-- Button -->
-
-                        <div class="col-sm-12 controls">
-                          <input id="submit" name="submit" type="submit" value="Submit" class"btn btn-primary"></a>
-
-                        </div>
                     </div>
+                </div>
                 </form>     
 
 
