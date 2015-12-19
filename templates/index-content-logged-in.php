e9fe7a1249a3d4954ceb02cbf64a95f17a099206
moxiewhimsy <kohler38@students.rowan.edu>
2015-12-19 18:56:12 -0500

    fixing elvis

--- templates/index-content-logged-in.php
+++ templates/index-content-logged-in.php
@@ -1,4 +1,6 @@
-<?php 
-	echo "<h3>Welcome, ". get_name($_SESSION['id']) ." </h3><br />";
+<div class="text-center">
+	<?php 
+		echo "<h3>Welcome, ". get_name($_SESSION['id']) ." </h3><br />";
 
-?>
+	?>
+</div>
