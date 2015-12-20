d30f51d6bd31ac20f38b827f7a2366bde487c3e1
moxiewhimsy <kohler38@students.rowan.edu>
2015-12-20 02:39:33 -0500

    debug login

--- core/database/login.php
+++ core/database/login.php
@@ -2,6 +2,10 @@
 	include '../init.php';
 	$link = connectDB();
 	if(empty($_POST) == false) {
+
+		if (empty($password)) {
+			$errors[] = 'password is empty before clean_up';
+		}
 		echo $_POST['username'] . " -> ";
 		$username = ($_POST['username']);
 		$password = ($_POST['password']);
@@ -9,6 +13,12 @@
 
 		$username = clean_up("$username");
 		$password = clean_up("$password");
+
+		echo "-> $username";
+
+		if (empty($password)) {
+			$errors[] = 'password is empty after clean_up';
+		}
 		
 		if(empty($username) == true || empty($password) == true) {
 			$errors[] = 'Enter a username and password';
